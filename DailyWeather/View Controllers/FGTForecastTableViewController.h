91150868eddd8264731fd5f851adddbae2c82fd4
fritzgt <tuanis@live.com>
2020-06-21 09:05:37 +0300

    Setting up cities list TVC

---
+++ DailyWeather/View Controllers/FGTForecastTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FGTForecastTableViewController.h
+//  EZWeather
+//
+//  Created by FGT MAC on 6/21/20.
+//  Copyright © 2020 Fritz Gamboa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FGTForecastTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
